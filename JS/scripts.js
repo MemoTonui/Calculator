1977dbabe9c64e9608b474d707a505ba2c8949f1
Linda Tonui <tonuilinda@gmail.com>
2020-04-30 13:27:56 +0300

    Add the JS code

---
+++ JS/scripts.js
@@ -0,0 +1,18 @@
+function add(number1, number2) {
+    return number1 + number2;
+  }
+  
+  function subtract(number1, number2) {
+    if (number1 > number2) {
+      return number1 - number2;
+    } else {
+      return number2 - number1;
+    }
+  }
+  var number1 = parseInt(prompt("Enter a number: "));
+  var number2 = parseInt(prompt("Enter another number: "));
+  
+  alert(add(number1, number2));
+  
+  alert(subtract(number1,number2));
+  
